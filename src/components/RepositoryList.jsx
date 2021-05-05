d37a82019713f78929a02652b1abfaaecc8913bc
Diogo Miranda <diogosminfor@gmail.com>
2021-05-05 14:30:00 -0300

    useEffect conceitos

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,20 +1,34 @@
+import {useState, useEffect} from 'react'
 import { RepositoryItem } from './RepositoryItem';
 
-const repository ={
-    name:'unform',
-    description: 'form in unform',
-    link: 'https://github.com/unform/unform'
-}
+import '../styles/repositories.scss';
+
+const repository = {
+  name: 'unform',
+  description: 'form in unform',
+  link: 'https://github.com/unform/unform',
+};
 export function RepositoryList() {
-   
+
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(()=>{
+
+    //acessando a api do git 
+    fetch('https://api.github.com/users/diogomiranda-dev/repos')
+    .then(response => response.json())
+    .then(data => setRepositories(data))
+
+  },[])
+
   return (
     <section className="repository-list">
       <h1>Lista de repositório</h1>
       <ul>
         <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
       </ul>
     </section>
   );
